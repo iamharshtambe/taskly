096f2594a2a56b0f0cc9acdac2e9e3f30ee3a9f1
iamharshtambe <harshtambe.dev@gmail.com>
2025-03-27 19:48:35 +0530

    built basic structure

---
+++ script.js
@@ -0,0 +1,22 @@
+const todoList = [];
+
+function renderTodos() {
+   let todoListHtml = '';
+
+   for (let i = 0; i < todoList.length; i++) {
+      todoListHtml += `<p>${todoList[i]}</>`;
+
+      document.querySelector('.js-tasks').innerHTML = todoListHtml;
+   }
+}
+
+function addTask() {
+   const inputElement = document.querySelector('.js-input');
+   const inputValue = inputElement.value;
+
+   todoList.push(inputValue);
+
+   renderTodos();
+
+   inputElement.value = '';
+}
