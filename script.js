652b1077466c303ec8bda88c64a7fd23bf7ac18d
iamharshtambe <harshtambe.dev@gmail.com>
2025-03-28 14:28:08 +0530

    added delete todo feature

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ function renderTodos() {
    let todoListHtml = '';
 
    for (let i = 0; i < todoList.length; i++) {
-      todoListHtml += `<p>${todoList[i]}</p>`;
-
-      document.querySelector('.js-tasks').innerHTML = todoListHtml;
+      todoListHtml += `
+      <p>
+         ${todoList[i]} 
+         <button onclick='
+            todoList.splice(${i}, 1);
+            renderTodos();
+         '>Delete</button>
+      </p>
+      `;
    }
+
+   document.querySelector('.js-tasks').innerHTML = todoListHtml;
 }
 
 function addTask() {
