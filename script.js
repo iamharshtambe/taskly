a6214525adb5bc77b20974ffac78f0a1ebac0369
iamharshtambe <harshtambe.dev@gmail.com>
2025-03-29 12:56:50 +0530

    added name and due date

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const todoList = [];
+const todoList = [
+   { name: 'do dinner', dueDate: '2025-3-29' },
+   { name: 'do dance', dueDate: '2025-3-29' },
+];
 
 function renderTodos() {
    let todoListHtml = '';
 
    for (let i = 0; i < todoList.length; i++) {
+      const todoObject = todoList[i];
+      const { name, dueDate } = todoObject;
+
       todoListHtml += `
       <p>
-         ${todoList[i]} 
+         ${name} ${dueDate} 
          <button onclick='
             todoList.splice(${i}, 1);
             renderTodos();
